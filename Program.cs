ae811ce9285f58cf21b03de9cfc960dc22d3f4ea
hecmarrr <hecmarrr@hotmail.com>
2026-03-27 00:45:04 -0500

    Enable Swagger without Microsoft.OpenApi.Models

--- Program.cs
+++ Program.cs
@@ -1,18 +1,8 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "SQL File API",
-        Version = "v1",
-        Description = "Servicio REST con un método para devolver un archivo SQL de ejemplo."
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
